7ce54a8a27dff29eb0fe48adf03d52c67f56dc96
alfredoqt <alfredoq98@hotmail.com>
2019-04-17 18:03:34 -0500

    Redoing plugin

--- ios/Classes/AwsAmplifyStoragePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AwsAmplifyStoragePlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/AwsAmplifyStoragePlugin.m
+++
@@ -1,8 +0,0 @@
-#import "AwsAmplifyStoragePlugin.h"
-#import <aws_amplify_storage/aws_amplify_storage-Swift.h>
-
-@implementation AwsAmplifyStoragePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftAwsAmplifyStoragePlugin registerWithRegistrar:registrar];
-}
-@end
